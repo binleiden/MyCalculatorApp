055def0189bb653f9e54f870c618b5dd9150e63b
binleiden <daniel.petersson83@gmail.com>
2024-02-29 09:31:04 +0100

    Defined structure of application

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -10,4 +10,21 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    //Display input options
+    //Addition
+    //Subtraction
+    //Multiplication
+    //Division
+    //Exit
+
+    //User select which method to use for calculation of input nr1 and nr2
+
+    //Enter nr1 Scanner input store in value nr1
+
+    //Enter nr2 Scanner input store in value nr2
+
+    //Calculate and display result and then go back to main menu
+
+
 }
