8bdebe5590cc2d7a9929b7b377c394cfca53f00b
binleiden <daniel.petersson83@gmail.com>
2024-02-29 10:06:46 +0100

    Menu output

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -8,23 +8,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-
     //Display input options
+        System.out.println( "My Calculator App" );
+
     //Addition
+        System.out.println("1: Addition");
+
     //Subtraction
+        System.out.println("1: Subtraction");
+
     //Multiplication
+        System.out.println("1: Multiplication");
+
     //Division
+        System.out.println("1: Division");
+
     //Exit
+        System.out.println("1: Exit");
 
     //User select which method to use for calculation of input nr1 and nr2
 
-    //Enter nr1 Scanner input store in value nr1
-
-    //Enter nr2 Scanner input store in value nr2
-
+    //Enter number1 Scanner input store in value nr1
+    int number1;
+    //Enter number2 Scanner input store in value nr2
+    int number2;
     //Calculate and display result and then go back to main menu
+    int result;
 
-
+    }
 }
