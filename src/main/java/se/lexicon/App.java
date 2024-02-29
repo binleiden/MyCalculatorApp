13fa106812272d7bfff205e8051fbce66884599c
binleiden <daniel.petersson83@gmail.com>
2024-02-29 10:53:11 +0100

    Created while loop with switch statement for calculation.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -21,22 +21,47 @@ public class App {
 
         //Division
         System.out.println("4: Division");
-
+        
         //Exit
         System.out.println("5: Exit");
-
+        int exit = 5;
         //User select which method to use for calculation of input nr1 and nr2
-
+        int i = 1;
+        while (true){
+            Scanner choice = new Scanner(System.in);
+            int userChoice = choice.nextInt();
+            Scanner firstNumber = new Scanner(System.in);
+            System.out.println("Enter first nr");
+            int number1 = firstNumber.nextInt();
+            Scanner secondNumber = new Scanner(System.in);
+            System.out.println("Enter second nr");
+            int number2 = secondNumber.nextInt();
+            switch (userChoice){
+                case 1:
+                    Object addition = number1 + number2;
+                    System.out.println(addition);
+                    break;
+                case 2:
+                    Object subtraction = number1 - number2;
+                    System.out.println(subtraction);
+                    break;
+                case 3:
+                    Object multiplication = number1 * number2;
+                    System.out.println(multiplication);
+                    break;
+                case 4:
+                    Object division = number1 / number2;
+                    System.out.println(division);
+                    break;
+            }
+        }
+       
         //Enter number1 Scanner input store in value nr1
-        Scanner firstNumber = new Scanner(System.in);
-        System.out.println("Enter first nr");
-        int number1 = firstNumber.nextInt();
+
         //Enter number2 Scanner input store in value nr2
-        Scanner secondNumber = new Scanner(System.in);
-        System.out.println("Enter second nr");
-        int number2 = secondNumber.nextInt();
+
         //Calculate and display result and then go back to main menu
-        int result;
+        //int result;
 
     }
 }
