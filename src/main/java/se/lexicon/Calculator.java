646c7b96e5d700887f3b8822cffeb25d308e0f37
binleiden <daniel.petersson83@gmail.com>
2024-02-29 13:19:53 +0100

    Created method for menu, And renamed application

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -5,29 +5,16 @@ import java.util.Scanner;
 /**
  * Hello world!
  */
-public class App {
+public class Calculator {
     public static void main(String[] args) {
-        //Display input options
-        System.out.println("My Calculator App");
-
-        //Addition
-        System.out.println("1: Addition");
-
-        //Subtraction
-        System.out.println("2: Subtraction");
-
-        //Multiplication
-        System.out.println("3: Multiplication");
-
-        //Division
-        System.out.println("4: Division");
-        
-        //Exit
-        System.out.println("5: Exit");
-        int exit = 5;
+        //Enter number1 Scanner input store in value nr1
+        //Enter number2 Scanner input store in value nr2
+        //Calculate and display result and then go back to main menu
+        //int result;
         //User select which method to use for calculation of input nr1 and nr2
         boolean startLoop = true;
         while (startLoop){
+            Calculator.displayMenu();
             Scanner choice = new Scanner(System.in);
             int userChoice = choice.nextInt();
             Scanner firstNumber = new Scanner(System.in);
@@ -56,15 +43,19 @@ public class App {
                 case 5:
                     startLoop = false;
                     break;
+                default:
+                    System.out.println("Select a number between 1 and 5");
             }
         }
-       
-        //Enter number1 Scanner input store in value nr1
 
-        //Enter number2 Scanner input store in value nr2
-
-        //Calculate and display result and then go back to main menu
-        //int result;
-
+    }
+    //Display input options
+    public static void displayMenu(){
+        System.out.println("My Calculator App");
+        System.out.println("1: Addition");
+        System.out.println("2: Subtraction");
+        System.out.println("3: Multiplication");
+        System.out.println("4: Division");
+        System.out.println("5: Exit");
     }
 }
